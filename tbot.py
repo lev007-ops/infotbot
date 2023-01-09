04d1b42b58e927e51405c348c4c5b0209e166e13
mb6ockatf <mb6ockatf@relay.firefox.com>
2023-01-09 23:14:52 +0300

    fix: typo, clean-up
     - now looks cleaner
     - unused imports removed
     - fits in pylint's demands

--- tbot.py
+++ tbot.py
@@ -1,10 +1,7 @@
+#!/usr/bin/env python3
 import telebot
-from telebot.types import Message
-import config
 from telebot import types
-
-
-
+import config
 
 
 client = telebot.TeleBot(config.CONFIG['token'])
@@ -18,11 +15,7 @@ def welcom(message):
     client.send_message(message.chat.id, hello_text)
 
 
-
 @client.message_handler(commands=['get_info', 'info'])
-
-
-
 
 
 def get_user_info(message):
@@ -30,10 +23,9 @@ def get_user_info(message):
     item_yes = types.InlineKeyboardButton(text = 'Да✅', callback_data='yes')
     item_no = types.InlineKeyboardButton(text = 'Нет❌', callback_data='no')
     markup_inline.add(item_yes, item_no)
+    chat_id = message.chat.id
+    client.send_message(chat_id, 'Получить инфу?', reply_markup=markup_inline)
 
-    client.send_message(message.chat.id, 'Получить инфу?',
-        reply_markup=markup_inline
-        )
 
 @client.callback_query_handler(func=lambda call: True)
 def answer(call):
@@ -42,19 +34,12 @@ def answer(call):
         item_id = types.KeyboardButton('Moй Id')
         item_username = types.KeyboardButton('Moй ник')
         markup_reply.add(item_id, item_username)
-        client.send_message(call.message.chat.id, 'Нажмите на кнопку для получения информации',
-            reply_markup=markup_reply
-        )
+        reply = "Нажмите на кнопку для получения информации"
+        chat_id = call.message.chat.id
+        client.send_message(chat_id, reply, reply_markup=markup_reply)
 
     elif call.data == 'no':
-        client.send_message(call.message.chat.id, 'Вругой раз')
-
-        
-
-    
-
-
-
+        client.send_message(call.message.chat.id, 'В другой раз')
 
 
 @client.message_handler(content_types=['text'])
@@ -62,11 +47,11 @@ def get_text(message):
     if message.text == 'Moй Id':
         client.send_message(message.chat.id, f'Ваш Id: {message.from_user.id}')
     elif message.text == 'Moй ник':
-        client.send_message(message.chat.id, f'Ваш ник: {message.from_user.first_name} {message.from_user.last_name}')
-
-
-
-
+        first_name = message.from_user.first_name
+        last_name = message.from_user.last_name
+        reply_text = f'Ваш ник: {first_name} {last_name}'
+        client.send_message(message.chat.id, reply_text)
 
 
 client.polling(none_stop=True, interval=0)
+
